0c2812876ee4531b4fcd6110331fb28f790a6f54
Dhanavandhan <dhanavandhan@capeindia.net>
2022-07-15 12:38:23 +0530

    changes for Bus Ticket Project

--- Dhanavandhan/reverse/com/Busticket.java
+++ Dhanavandhan/reverse/com/Busticket.java
@@ -4,90 +4,104 @@ import java.util.Scanner;
 
 public class Busticket {
 	double ticketPrice;
-public static void main(String []args) {
-	
-	Scanner sc=new Scanner(System.in);
-	Busticket a=new Busticket();
-	
-	System.out.println("this bus are only five stops:"
-			+ "1)thiruvannamalai"
-			+"   "+ "2)gingee"
-			+"   "+ "3)Thindivanam"
-			+"   "+ "4)chengalpattu"
-			+"   "+ "5)perungulathur ");
-	
-	System.out.println("enter which Place you Want ticket:");
-	
-	String f=sc.next();
-	switch(f) {
-	
-	case "thiruvannamalai":
-	
-		a.ticketPrice=175;
-		System.out.println("your ticket price:"+a.ticketPrice);
-		
-	break;
-	case "Gingee":
-	
-		a.ticketPrice=133;
-		System.out.println("your ticket price:"+a.ticketPrice);
-		break;
-	case "Thindivanam":
-		a.ticketPrice=120;
-		System.out.println("your ticket price:"+a.ticketPrice);
-		break;
-	case "chengalpattu":
-		a.ticketPrice=75;
-		System.out.println("your ticket price:"+a.ticketPrice);
-		break;
-	case "perungulathur":
-		a.ticketPrice=35;
-		System.out.println("your ticket price:"+a.ticketPrice);
-		break;
-	default:
-		System.out.println("The Stop Not Available in Our Bus Services");
-	 
-		break;
-	}
-	
-	int n;
-	
-	double give,remain,change,totalamount;
-	Busticket j=new Busticket();
-	System.out.println("enter how many Tickets youwant");
-	n=sc.nextInt();
-	totalamount=n*a.ticketPrice;
-	System.out.println("yor total Amount:"+totalamount);
-	System.out.println("Amount you give:");
-	give = sc.nextDouble();
-	if (give < totalamount) 
-	{
-		remain = totalamount - give;
-		System.out.println("Please give remain balance are:" + remain);
-		System.out.println("Enter you give amount:");
-		give = sc.nextDouble();
-		if (give > remain)
-		{
-			change = give - remain;
-			System.out.println("your change please collect amount:" + change);
-			System.out.println("thanks for travel our bus");
-		}else if(give == remain) {
-			System.out.println("thanks for travel our bus");
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Busticket a = new Busticket();
+
+		for (int f = 1; f < 6; f++) {
+			System.out.println("this bus are only five stops:" + "1)thiruvannamalai" + "   " + "2)gingee" + "   "
+					+ "3)Thindivanam" + "   " + "4)chengalpattu" + "   " + "5)perungulathur ");
+
+			System.out.println("press 1 to book ticket thiruvannamalai");
+			System.out.println("press 2 to book ticket gingee");
+			System.out.println("press 3 to book ticket Thindivanam");
+			System.out.println("press 4 to book ticket chengalpattu");
+			System.out.println("press 5 to book ticket perungulathur");
+			System.out.println("press 6 to Exit");
+
+			f = sc.nextInt();
+
+			switch (f) {
+
+			case 1:
+
+				a.ticketPrice = 175;
+				System.out.println("your ticket price:" + a.ticketPrice);
+
+				break;
+			case 2:
+
+				a.ticketPrice = 133;
+				System.out.println("your ticket price:" + a.ticketPrice);
+				break;
+			case 3:
+				a.ticketPrice = 120;
+				System.out.println("your ticket price:" + a.ticketPrice);
+				break;
+			case 4:
+				a.ticketPrice = 75;
+				System.out.println("your ticket price:" + a.ticketPrice);
+				break;
+			case 5:
+				a.ticketPrice = 35;
+				System.out.println("your ticket price:" + a.ticketPrice);
+				break;
+			default:
+
+			}
+
+			if (f == 6) {
+
+				System.out.println("thank you");
+				// TODO Auto-generated method stub
+			} else if (f < 1) {
+				System.out.println("You enter Wrong Option");
+			}
+
+			else if (f < 6) {
+				a.display(sc, a);
+
+			}
+
+			else {
+				System.out.println("You enter Wrong Option");
+			}
 		}
-	} 
-	else if (give == totalamount)
-	{
-		System.out.println("thanks for travel our Bus");
-		
+	}
 
-	} else
-	{
-		change = give - totalamount;
-		System.out.println("your change is :" + change);
-		System.out.println("thanks for travel our Bus");
-	
-}
-	// TODO Auto-generated method stub
+	private void display(Scanner sc, Busticket a) {
+		int n;
 
-}
+		double give, remain, change, totalamount;
+		Busticket j = new Busticket();
+		System.out.println("enter how many Tickets youwant");
+		n = sc.nextInt();
+		totalamount = n * a.ticketPrice;
+		System.out.println("yor total Amount:" + totalamount);
+		System.out.println("Amount you give:");
+		give = sc.nextDouble();
+		if (give < totalamount) {
+			remain = totalamount - give;
+			System.out.println("Please give remain balance are:" + remain);
+			System.out.println("Enter you give amount:");
+			give = sc.nextDouble();
+			if (give > remain) {
+				change = give - remain;
+				System.out.println("your change please collect amount:" + change);
+				System.out.println("thanks for travel our bus");
+			} else if (give == remain) {
+				System.out.println("thanks for travel our bus");
+			}
+		} else if (give == totalamount) {
+			System.out.println("thanks for travel our Bus");
+
+		} else {
+			change = give - totalamount;
+			System.out.println("your change is :" + change);
+			System.out.println("thanks for travel our Bus");
+
+		}
+	}
 }
